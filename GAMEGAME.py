d8135da08692bfa112c770903c4d460992318218
Adrian annies dator <adrian.eliasson@hotmail.com>
2023-11-07 22:40:57 +0100

    healthbar och bullet försök

--- GAMEGAME.py
+++ GAMEGAME.py
@@ -13,6 +13,14 @@ player_x = 600
 player_y = 400
 player_rect = pygame.Rect(player_x, player_y, 40, 40)
 
+player_health_x = player_x - 7
+player_health_y = player_y + 46
+player_health_width = 54
+player_health_height = 7
+player_health_rect = pygame.Rect(player_health_x, player_health_y, player_health_width, player_health_height)
+
+player_health_rect_red = pygame.Rect(player_health_x, player_health_y, 54, player_health_height)
+
 enemy1_x = 300
 enemy1_y = 700
 enemy_rect = pygame.Rect(enemy1_x, enemy1_y, 30, 30)
@@ -21,7 +29,7 @@ surrounding1_x = random.randint(0, 1140)
 surrounding1_y = random.randint(0, 1040)
 
 surrounding2_x = random.randint(0, 1130)
-surrounding2_y = random.randint(0, 1300)
+surrounding2_y = random.randint(0, 1030)
 
 left_wall_top_x = 0
 left_wall_top_y = 0
@@ -47,14 +55,29 @@ inside_x = 0
 inside_y = 0
 inside_rect = pygame.Rect(inside_x, inside_y, 1200, 1100)
 
+bullets = []
 
+class Bullet():
+    def __init__(self, bullet_x, bullet_y, bullet_speed):    
+        self.bullet_x = bullet_x
+        self.bullet_y = bullet_y
+        self.bullet_speed
+    def draw_bullet(self, surface):
+        bullet_dx, bullet_dy = cursor_x - player_x, cursor_y - player_y
+        bullet_dist = math.hypot(bullet_dx, bullet_dy)
+        bullet_dx, bullet_dy = bullet_dx / bullet_dist, bullet_dy / bullet_dist
+        pygame.draw.rect(display, bullet_color, bullet_rect)
+#bullet_width_height = 3
+bullet_rect = pygame.Rect(bullet_x + 20, bullet_y + 20, bullet_width_height, bullet_width_height)
 
 color_player = (47, 94, 161)
 enemy_color = (102, 3, 3)
 surrounding_color = (22, 71, 15)
 wall_color = (72, 74, 72)
-inside_color = (70, 107, 50)
-bullet_color = (40, 58, 156)
+inside_color = (65, 122, 54)
+#bullet_color = (40, 58, 156)
+health_color_red = (100, 0, 0)
+health_color_green = (38, 196, 8)
 
 clock = pygame.time.Clock()
 
@@ -130,6 +153,19 @@ while game_over == False:
     inside_rect = pygame.Rect(inside_x, inside_y, 1200, 1100)
     cursor_x, cursor_y = pygame.mouse.get_pos()
 
+    if player_rect.colliderect(enemy_rect):
+        player_health_width -= 0.35
+    if player_health_width < 35:
+        health_color_green = (182, 191, 0)
+    if player_health_width < 20:
+        health_color_green = (204, 22, 2)
+
+    if pygame.MOUSEBUTTONDOWN == True:
+        bullet.draw_bullet(display)
+
+    
+    player_health_rect = pygame.Rect(player_health_x, player_health_y, player_health_width, player_health_height)
+
     display.fill((30, 20, 0))
 
     pygame.draw.rect(display, inside_color, inside_rect)
@@ -138,6 +174,8 @@ while game_over == False:
     pygame.draw.rect(display, enemy_color, enemy_rect)
     pygame.draw.line(display, bullet_color, (player_x + 20, player_y + 20), (cursor_x, cursor_y), 5)
     pygame.draw.rect(display, color_player, player_rect)
+    pygame.draw.rect(display, health_color_red, player_health_rect_red)
+    pygame.draw.rect(display, health_color_green, player_health_rect)
     pygame.draw.line(display, wall_color, (left_wall_top_x, left_wall_top_y), (left_wall_bottom_x, left_wall_bottom_y), 10)
     pygame.draw.line(display, wall_color, (top_wall_left_x, top_wall_left_y), (top_wall_right_x, top_wall_right_y), 10)
     pygame.draw.line(display, wall_color, (right_wall_top_x, right_wall_top_y), (right_wall_bottom_x, right_wall_bottom_y), 10)
