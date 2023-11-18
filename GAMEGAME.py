68eaf8a83e1763f223f46306c5d963ecf34fab76
adrian <adreli@student.chalmers.se>
2023-11-18 16:03:40 +0100

    Projectiles

--- GAMEGAME.py
+++ GAMEGAME.py
@@ -1,6 +1,25 @@
+from dataclasses import dataclass
+from typing import List
 import pygame
 import random
 import math
+
+
+@dataclass
+class Color:
+    r: int
+    g: int
+    b: int
+
+    def get_color(self) -> (int, int, int):
+        return (self.r, self.g, self.b)
+
+
+@dataclass
+class Coordinate:
+    x: int
+    y: int
+
 
 pygame.init()
 
@@ -17,9 +36,11 @@ player_health_x = player_x - 7
 player_health_y = player_y + 46
 player_health_width = 54
 player_health_height = 7
-player_health_rect = pygame.Rect(player_health_x, player_health_y, player_health_width, player_health_height)
+player_health_rect = pygame.Rect(
+    player_health_x, player_health_y, player_health_width, player_health_height)
 
-player_health_rect_red = pygame.Rect(player_health_x, player_health_y, 54, player_health_height)
+player_health_rect_red = pygame.Rect(
+    player_health_x, player_health_y, 54, player_health_height)
 
 enemy1_x = 300
 enemy1_y = 700
@@ -60,20 +81,39 @@ projectile_x = cursor_x
 projectile_y = cursor_y
 projectile_rect = pygame.Rect(projectile_x, projectile_y, 20, 20)
 
-#bullets = []
+click_count = 0
 
-#class Bullet():
- #   def __init__(self, bullet_x, bullet_y, bullet_speed):    
-  #      self.bullet_x = bullet_x
-   #     self.bullet_y = bullet_y
-    #    self.bullet_speed
-    #def draw_bullet(self, surface):
-     #   bullet_enemy_dx, bullet_enemy_dy = cursor_x - player_x, cursor_y - player_y
-      #  bullet_enemy_dist = math.hypot(bullet_enemy_dx, bullet_enemy_dy)
-       # bullet_enemy_dx, bullet_enemy_dy = bullet_enemy_dx / bullet_enemy_dist, bullet_enemy_dy / bullet_enemy_dist
-        #pygame.draw.rect(display, bullet_color, bullet_rect)
-#bullet_width_height = 3
-#bullet_rect = pygame.Rect(bullet_x + 20, bullet_y + 20, bullet_width_height, bullet_width_height)
+# Reload Timers
+RELOAD_DELAY_TYPE1 = 600   # milliseconds
+last_shot = pygame.time.get_ticks()
+
+
+class Projectile():
+    def __init__(self, start_pos: Coordinate, direction: float, speed: int, color: Color):
+        self.size = (20, 20)
+        self.direction = direction  # Angle `theta`
+        self.speed = speed
+        self.color = color
+        self.hitbox = pygame.rect.Rect(start_pos, self.size)
+
+    def update(self):
+        diff_x = math.cos(self.direction) * self.speed
+        diff_y = math.sin(self.direction) * self.speed
+        self.hitbox.x += diff_x
+        self.hitbox.y += diff_y
+        return self
+
+    def draw(self, display):
+        pygame.draw.rect(display, (self.color.r, self.color.g,
+                         self.color.b), self.hitbox)
+        return self
+
+    def move(self, diff_x, diff_y):
+        self.hitbox.x += diff_x
+        self.hitbox.y += diff_y
+
+
+projectiles: List[Projectile] = []
 
 color_player = (47, 94, 161)
 enemy_color = (102, 3, 3)
@@ -83,32 +123,7 @@ inside_color = (65, 122, 54)
 projectile_color = (40, 58, 156)
 health_color_red = (100, 0, 0)
 health_color_green = (38, 196, 8)
-
-objects_x = [surrounding1_x, surrounding2_x, enemy1_x, left_wall_top_x, left_wall_bottom_x, top_wall_right_x, top_wall_left_x, right_wall_top_x, right_wall_bottom_x, bottom_wall_right_x, bottom_wall_left_x, inside_x, projectile_x]
-objects_y = [surrounding1_y, surrounding2_y, enemy1_y, left_wall_top_y, left_wall_bottom_y, top_wall_right_y, top_wall_left_y, right_wall_top_y, right_wall_bottom_y, bottom_wall_right_y, bottom_wall_left_y, inside_y, projectile_y]
-
-cursor_x, cursor_y = pygame.mouse.get_pos()
-cursor_rect = pygame.Rect(cursor_x - 10, cursor_y - 10, 20, 20)
-projectile_rect = pygame.Rect(projectile_x, projectile_y, 20, 20)
-
-projectile_list = []
-
-def projectile():
-    global projectile_x
-    global projectile_y
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        pygame.draw.rect(display, projectile_color, cursor_rect)
-        projectile_dx, projectile_dy = projectile_x - player_x, projectile_y - player_y
-        projectile_dist = math.hypot(projectile_dx, projectile_dy)
-        projectile_dx, projectile_dy = projectile_dx / projectile_dist, projectile_dy / projectile_dist
-        projectile_x += projectile_dx
-        projectile_y += projectile_dy
-        projectile_rect = pygame.Rect(projectile_x, projectile_y, 20, 20)
-        #projectile_list.append(projectile_rect)
-        #for i in projectile_list:
-        pygame.draw.rect(display, projectile_color, projectile_rect)
-        #projectile_list.pop(0)
-        
+projectile_color = Color(47, 94, 161)
 
 clock = pygame.time.Clock()
 
@@ -134,6 +149,7 @@ while game_over == False:
         bottom_wall_left_x -= 3
         inside_x -= 3
         projectile_x -= 3
+        [p.move(-3, 0) for p in projectiles]
     if keys[pygame.K_a]:
         surrounding1_x += 3
         surrounding2_x += 3
@@ -148,6 +164,8 @@ while game_over == False:
         bottom_wall_left_x += 3
         inside_x += 3
         projectile_x += 3
+        [p.move(3, 0) for p in projectiles]
+
     if keys[pygame.K_s]:
         surrounding1_y -= 3
         surrounding2_y -= 3
@@ -162,10 +180,12 @@ while game_over == False:
         bottom_wall_left_y -= 3
         inside_y -= 3
         projectile_y -= 3
+        [p.move(0, -3) for p in projectiles]
+
     if keys[pygame.K_w]:
         surrounding1_y += 3
         surrounding2_y += 3
-        enemy1_y += 3   
+        enemy1_y += 3
         left_wall_top_y += 3
         left_wall_bottom_y += 3
         top_wall_right_y += 3
@@ -176,21 +196,19 @@ while game_over == False:
         bottom_wall_left_y += 3
         inside_y += 3
         projectile_y += 3
+        [p.move(0, 3) for p in projectiles]
 
-    enemy_dx, enemy_dy = player_x - enemy1_x, player_y - enemy1_y
-    enemy_dist = math.hypot(enemy_dx, enemy_dy)
-    enemy_dx, enemy_dy = enemy_dx / enemy_dist, enemy_dy / enemy_dist
+    dx, dy = player_x - enemy1_x, player_y - enemy1_y
+    dist = math.hypot(dx, dy)
+    dx, dy = dx / dist, dy / dist
 
-    enemy1_x += enemy_dx
-    enemy1_y += enemy_dy
+    enemy1_x += dx
+    enemy1_y += dy
 
     enemy_rect = pygame.Rect(enemy1_x, enemy1_y, 30, 30)
     inside_rect = pygame.Rect(inside_x, inside_y, 1200, 1100)
-    
     cursor_x, cursor_y = pygame.mouse.get_pos()
-    cursor_rect = pygame.Rect(cursor_x - 10, cursor_y - 10, 20, 20)
-
-    
+    projectile_rect = pygame.Rect(projectile_x, projectile_y, 20, 20)
 
     if player_rect.colliderect(enemy_rect):
         player_health_width -= 0.35
@@ -198,29 +216,57 @@ while game_over == False:
         health_color_green = (182, 191, 0)
     if player_health_width < 20:
         health_color_green = (204, 22, 2)
-    
-    player_health_rect = pygame.Rect(player_health_x, player_health_y, player_health_width, player_health_height)
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if pygame.mouse.get_pressed():
+            now = pygame.time.get_ticks()
+            if (now > last_shot + RELOAD_DELAY_TYPE1):
+                # Fire gun
+                (dx, dy) = pygame.mouse.get_pos() - \
+                    pygame.Vector2(player_rect.center)
+                angle = math.atan2(dy, dx)
+                projectile = Projectile(
+                    (player_rect.center), angle, 3, projectile_color)
+                projectiles.append(projectile)
+                # Reset reload timer
+                last_shot = now
+            else:
+                # No shot
+                pass
+
+ #   if pygame.MOUSEBUTTONDOWN == True:
+  #      bullet.draw_bullet(display)
+
+    player_health_rect = pygame.Rect(
+        player_health_x, player_health_y, player_health_width, player_health_height)
 
     display.fill((30, 20, 0))
 
     pygame.draw.rect(display, inside_color, inside_rect)
-    pygame.draw.rect(display, surrounding_color, (surrounding1_x, surrounding1_y, 60, 60))
-    pygame.draw.rect(display, surrounding_color, (surrounding2_x, surrounding2_y, 70, 70))
+    pygame.draw.rect(display, surrounding_color,
+                     (surrounding1_x, surrounding1_y, 60, 60))
+    pygame.draw.rect(display, surrounding_color,
+                     (surrounding2_x, surrounding2_y, 70, 70))
     pygame.draw.rect(display, enemy_color, enemy_rect)
-   # pygame.draw.line(display, bullet_color, (player_x + 20, player_y + 20), (cursor_x, cursor_y), 5)
-    #pygame.draw.rect(display, projectile_color, projectile_rect)
+    # pygame.draw.line(display, bullet_color, (player_x + 20, player_y + 20), (cursor_x, cursor_y), 5)
+    # pygame.draw.rect(display, projectile_color, projectile_rect)
     pygame.draw.rect(display, color_player, player_rect)
-    pygame.draw.line(display, wall_color, (left_wall_top_x, left_wall_top_y), (left_wall_bottom_x, left_wall_bottom_y), 10)
-    pygame.draw.line(display, wall_color, (top_wall_left_x, top_wall_left_y), (top_wall_right_x, top_wall_right_y), 10)
-    pygame.draw.line(display, wall_color, (right_wall_top_x, right_wall_top_y), (right_wall_bottom_x, right_wall_bottom_y), 10)
-    pygame.draw.line(display, wall_color, (bottom_wall_left_x, bottom_wall_left_y), (bottom_wall_right_x, bottom_wall_right_y), 10)
     pygame.draw.rect(display, health_color_red, player_health_rect_red)
     pygame.draw.rect(display, health_color_green, player_health_rect)
-    
-    projectile()
+    pygame.draw.line(display, wall_color, (left_wall_top_x,
+                     left_wall_top_y), (left_wall_bottom_x, left_wall_bottom_y), 10)
+    pygame.draw.line(display, wall_color, (top_wall_left_x,
+                     top_wall_left_y), (top_wall_right_x, top_wall_right_y), 10)
+    pygame.draw.line(display, wall_color, (right_wall_top_x,
+                     right_wall_top_y), (right_wall_bottom_x, right_wall_bottom_y), 10)
+    pygame.draw.line(display, wall_color, (bottom_wall_left_x,
+                     bottom_wall_left_y), (bottom_wall_right_x, bottom_wall_right_y), 10)
 
+    pygame.draw.line(display, (60, 60, 60), player_rect.center,
+                     pygame.mouse.get_pos(), 5)
+    [p.update().draw(display) for p in projectiles]
     pygame.display.update()
 
-    clock.tick(100)
+    clock.tick(40)
 
 pygame.quit()
