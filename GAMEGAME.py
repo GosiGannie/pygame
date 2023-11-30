71f77e1c73565902649527ac72969aa5ab45b079
Annie Eliasson <anneli03@edu.umea.se>
2023-11-30 14:28:41 +0100

    enemy movement, småfixar

--- GAMEGAME.py
+++ GAMEGAME.py
@@ -32,12 +32,9 @@ enemy_color = Color(102, 3, 3)
 surrounding_color = (22, 71, 15)
 wall_color = (72, 74, 72)
 inside_color = (65, 122, 54)
-health_color_red = (100, 0, 0)
-health_color_green = (38, 196, 8)
+health_color_red = Color(100, 0, 0)
+health_color_green = Color(38, 196, 8)
 projectile_color = Color(47, 94, 161)
-
-
-#playerwhatewe
 
 
 #Surroundings
@@ -86,18 +83,42 @@ inside_y = -100
 
 
 class Player:
-    def __init__(self, x: int, y: int, width: int, height: int, color: Color):
+    def __init__(self, x: int, y: int, width: int, height: int, player_color: Color, max_hp: int, current_hp: int, health_bar_length: int, health_bar_color: Color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.color = color
+        self.player_color = player_color
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.max_hp = max_hp
+        self.current_hp = current_hp
+        self.health_bar_lenth = health_bar_length
+        self.health_ratio = self.max_hp / self.health_bar_lenth
+        self.health_bar_color = health_bar_color
     
     def draw(self, display):
-        pygame.draw.rect(display, (self.color.r, self.color.g, self.color.b), self.rect)
+        pygame.draw.rect(display, (self.player_color.r, self.player_color.g, self.player_color.b), self.rect)
 
-player = Player(600, 450, 40, 40, color_player)
+    def take_damage(self, amount: int):
+        if self.current_hp > 0:
+            self.current_hp -= amount
+        if self.current_hp <= 0:
+            self.current_hp = 0
+
+    def get_healing(self, amount: float):
+        if self.current_hp < self.max_hp:
+            self.current_hp += amount
+        if self.current_hp >= self.max_hp:
+            self.current_hp = self.max_hp
+
+    def draw_health_bar(self, display):
+        pygame.draw.rect(display, (self.health_bar_color.r, self.health_bar_color.g, self.health_bar_color.b), (20, 20, self.current_hp / self.health_ratio, 35))
+        pygame.draw.rect(display, (255, 255, 255), (20, 20, self.health_bar_lenth, 35), 4)
+
+player = Player(600, 450, 50, 50, color_player, 400, 400, 400, health_color_green)
+
+RELOAD_DELAY_TYPE3 = 2000
+last_healing = pygame.time.get_ticks()
 
 
 class Enemy():
@@ -133,12 +154,13 @@ last_enemy_spawn = pygame.time.get_ticks()
 
 
 class Projectile():
-    def __init__(self, start_pos: Coordinate, direction: float, speed: int, color: Color):
-        self.size = (15, 15)
+    def __init__(self, x: int, y: int, direction: float, speed: int, color: Color):
+        self.x = x
+        self.y = y
         self.direction = direction
         self.speed = speed
         self.color = color
-        self.hitbox = pygame.rect.Rect(start_pos, self.size)
+        self.hitbox = pygame.rect.Rect((self.x + display_scroll[0], self.y + display_scroll[1], 15, 15))
     
     def draw(self, display):
         pygame.draw.rect(display, (self.color.r, self.color.g, self.color.b), self.hitbox)
@@ -193,15 +215,14 @@ while game_over == False:
 
     now = pygame.time.get_ticks()
 
-    #Enemy moveent
-    if RELOAD_DELAY_TYPE2 < now + last_enemy_spawn:
-        enemy = Enemy(random.randint(-100, 1200), random.randint(-100, 900), 30, 30, enemy_color, 0.0, 3)
-        (enemy_dx, enemy_dy) = (enemy.x, enemy.y) - pygame.Vector2(600, 450)
+    #Enemy movement
+    if now - last_enemy_spawn > RELOAD_DELAY_TYPE2:        
+        enemy = Enemy(random.randint(-100, 1200), random.randint(-100, 900), 30, 30, enemy_color, 0.0, 2)
+        (enemy_dx, enemy_dy) = (enemy.x, enemy.y) - pygame.Vector2(600 - display_scroll[0], 450 - display_scroll[1])
         enemy_angle = math.atan2(enemy_dy, enemy_dx)
         enemy.direction = enemy_angle
-        #enemy = Enemy(random.randint(-100, 1200), random.randint(-100, 900), 30, 30, enemy_color, enemy_angle, 3)
         enemies.append(enemy)
-        now = last_enemy_spawn
+        last_enemy_spawn = now
 
     else:
         pass
@@ -209,6 +230,17 @@ while game_over == False:
 
 
     inside_rect = pygame.Rect(inside_x + display_scroll[0], inside_y + display_scroll[1], 1400, 1100)
+
+    for enemy in enemies:
+        if player.rect.colliderect(enemy.hitbox):
+            player.take_damage(1)
+            player.draw_health_bar(display)
+        
+    if now - last_healing > RELOAD_DELAY_TYPE3:
+        player.get_healing(0.1)
+
+    if player.current_hp < 300:
+        health_color_green = (204, 22, 2)
 
     #if player_rect.colliderect(enemy_rect):
     #    player_health_width -= 0.35
@@ -220,6 +252,17 @@ while game_over == False:
 
 
     #prijectile movement
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if pygame.mouse.get_pressed():
+            now = pygame.time.get_ticks()
+            if (now > last_shot + RELOAD_DELAY_TYPE1):
+                (dx, dy) = pygame.mouse.get_pos() - pygame.Vector2(player.rect.center)
+                angle = math.atan2(dy, dx)
+                projectile = Projectile(player.rect.x, player.rect.y, angle, 3, projectile_color)
+                projectiles.append(projectile)
+                last_shot = now
+            else:
+                pass
 
 
     #Draws
@@ -234,14 +277,11 @@ while game_over == False:
     for enemy in enemies:
         enemy.update().draw(display)
     player.draw(display)
-    pygame.draw.line(display, wall_color, (left_wall_top_x + display_scroll[0], left_wall_top_y + display_scroll[1]), (left_wall_bottom_x + display_scroll[0], left_wall_bottom_y + display_scroll[1]), 10)
-    pygame.draw.line(display, wall_color, (top_wall_left_x + display_scroll[0], top_wall_left_y + display_scroll[1]), (top_wall_right_x + display_scroll[0], top_wall_right_y + display_scroll[1]), 10)
-    pygame.draw.line(display, wall_color, (right_wall_top_x + display_scroll[0], right_wall_top_y + display_scroll[1]), (right_wall_bottom_x + display_scroll[0], right_wall_bottom_y + display_scroll[1]), 10)
-    pygame.draw.line(display, wall_color, (bottom_wall_left_x + display_scroll[0], bottom_wall_left_y + display_scroll[1]), (bottom_wall_right_x + display_scroll[0], bottom_wall_right_y + display_scroll[1]), 10)
-   # pygame.draw.rect(display, health_color_red, player_health_rect_red)
-   # pygame.draw.rect(display, health_color_green, player_health_rect)
-    [p.update().draw(display) for p in projectiles]
-
+    pygame.draw.rect(display, wall_color, (-100 + display_scroll[0], -100 + display_scroll[1], 1400, 1100), 10)
+    pygame.draw.rect(display, (255, 0, 0), (20, 20, 400, 35))
+    player.draw_health_bar(display)
+    for p in projectiles:
+        p.update().draw(display)
 
     pygame.display.update()
 
